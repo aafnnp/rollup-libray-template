64c87f286ced8aa2996fbc47c9558ebcccc61b04
pfan <gemini0525@foxmail.com>
2022-07-30 15:41:06 +0800

    chore: init

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,42 @@
-import typescript from 'rollup-plugin-typescript2';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import babel from '@rollup/plugin-babel';
-import {terser} from 'rollup-plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import postcss from 'rollup-plugin-postcss'
+import { terser } from 'rollup-plugin-terser'
+import image from '@rollup/plugin-image'
+import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
+import { module } from './package.json'
+import url from 'postcss-url'
 
-import pkg from './package.json';
-
-const extensions = ['.js', '.jsx', '.ts', '.tsx'];
-
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: pkg.main,
-      format: 'umd',
-      name: 'ComoNorth',
-    },
-    {
-      file: pkg.module,
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: module,
       format: 'es',
+      sourcemap: false,
     },
-  ],
-  plugins: [
-    typescript({
-      rollupCommonJSResolveHack: true,
-      clean: true,
-    }),
-    babel({
-      exclude: 'node_modules/**',
-      extensions,
-    }),
-    resolve(),
-    commonjs(),
-    terser(),
-  ],
-};
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      image(),
+      postcss({
+        plugins: [url({ url: 'inline' })],
+        extract: 'index.esm.css',
+        minimize: true,
+        sourceMap: false,
+      }),
+      terser(),
+    ],
+    external: ['react', 'react-dom', 'dayjs', 'palmpay-utils'],
+  },
+  {
+    input: 'dist/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+    external: [/\.s?css$/, /\.svg$/, /\.png$/, /\.jpg$/, /\.jpeg$/, /\.gif$/],
+  },
+]
